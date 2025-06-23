20291f5e757a84ae111d32d9254441708ecf937d
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2025-06-23 08:47:43 +0100

    Update app.js

--- public/app.js
+++ public/app.js
@@ -69,6 +69,9 @@ class QuizApp {
         this.users = [];
         this.leaderboard = [];
         this.init();
+        this.initImageUpload();
+        this.initThemeToggle();
+        
     }
 
     init() {
